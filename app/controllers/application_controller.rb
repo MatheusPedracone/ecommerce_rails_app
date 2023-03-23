41b9181134c7d4eae638e82a7db7372e36d7a292
Matheus Augusto Pedracone <matheuspedracone@gmail.com>
2023-03-23 20:40:19 -0300

    Add name and phone to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
     def destroy
         sign_out_and_redirect(current_user)
     end
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :email, :password, :password_confirmation, :phone)
+      end
+    end
+
 end
