ba2bf22d29a37d0f699bd218eb64860cbb60d7ed
Matheus Augusto Pedracone <matheuspedracone@gmail.com>
2023-03-29 09:39:20 -0300

    Install admin layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do 
+    root to: "home#index"
+  end
 end
